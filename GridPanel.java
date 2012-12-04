38d261cc8f0a1a0bece473a164e78c8ff53412c0
Chris Hilleren <ch.hill.001@gmail.com>
2012-12-04 01:53:48 -0800

    Add ability to generate any rule

--- GridPanel.java
+++ GridPanel.java
@@ -7,52 +7,42 @@ import javax.swing.JPanel;
 
 
 public class GridPanel extends JPanel{
-	private int blockSize = 5; //pixel width and height
-	private int gridWidth = 100; //width in blocks
-	private int gridHeight = 150; //height in blocks
+	private int blockSize = 3; //pixel width and height
+	private int gridWidth = 250; //width in blocks
+	private int gridHeight = 220; //height in blocks
 	private int componentWidth = gridWidth * blockSize; //width of JPanel in pixels
 	private int componentHeight = gridHeight * blockSize; //height of JPanel in pixels
 	
+	public int ruleNumber = 11;	
 	public int[] initialRow = new int[gridWidth];
 	
 	public GridPanel(){
 		super();
 		setPreferredSize(new Dimension(componentWidth, componentHeight));
 		setFocusable(true);
+		initialRow[initialRow.length / 2] = 1;
 		randomizeInitialRow();
 	}
 	
 	public void paintComponent(Graphics g){
-		
 		g.setColor(Color.black);
-		/*
-		//draw vertical lines
-		for(int i = 0; i < componentWidth; i += blockSize){
-			g.drawLine(i, 0, i, componentHeight);
-		}
 		
-		//draw horizontal lines
-		for(int i = 0; i < componentHeight; i += blockSize){
-			g.drawLine(0, i, componentWidth, i);
-		}
-		*/
 		//fill in the first row
 		for(int i = 0; i < gridWidth; i++){
 			if(initialRow[i] == 1){
 				g.fillRect(i * blockSize, 0, blockSize, blockSize);
 			}
-		}
-		
+		}		
 		
 		//fill in the rest of the grid
-		int[] nextRow = Rules.rule3(initialRow);
+		int[] nextRow = Rules.anyRule(ruleNumber, initialRow);
 		for(int i = 1; i < gridHeight; i++){
 			for(int j = 0; j < gridWidth; j++){
 				if(nextRow[j] == 1){
 					g.fillRect(j * blockSize, i * blockSize, blockSize, blockSize);
 				}
 			}
-			nextRow = Rules.rule3(nextRow);
+			nextRow = Rules.anyRule(ruleNumber, nextRow);
 		}
 	}
 	
--- Rules.java
+++ Rules.java
@@ -1,6 +1,41 @@
 
 public class Rules {
 	
+	//rule number must be between 0-255 inclusive
+	public static int[] anyRule(int ruleNumber, int[] previousRow){
+		String ruleBinary = Integer.toBinaryString(ruleNumber);
+		int[] nextRow = new int[previousRow.length];
+		
+		//pad to 8 digits
+		while(ruleBinary.length() < 8){
+			ruleBinary = "0" + ruleBinary;
+		}
+		
+		//borders are always 0
+		nextRow[0] = 0;
+		nextRow[nextRow.length - 1] = 0;
+		for(int i = 1; i < previousRow.length - 1; i++){
+			if(previousRow[i-1] == 1 && previousRow[i] == 1 && previousRow[i+1] == 1){
+				nextRow[i] = Character.getNumericValue(ruleBinary.charAt(0));
+			}else if(previousRow[i-1] == 1 && previousRow[i] == 1 && previousRow[i+1] == 0){
+				nextRow[i] = Character.getNumericValue(ruleBinary.charAt(1));
+			}else if(previousRow[i-1] == 1 && previousRow[i] == 0 && previousRow[i+1] == 1){
+				nextRow[i] = Character.getNumericValue(ruleBinary.charAt(2));
+			}else if(previousRow[i-1] == 1 && previousRow[i] == 0 && previousRow[i+1] == 0){
+				nextRow[i] = Character.getNumericValue(ruleBinary.charAt(3));
+			}else if(previousRow[i-1] == 0 && previousRow[i] == 1 && previousRow[i+1] == 1){
+				nextRow[i] = Character.getNumericValue(ruleBinary.charAt(4));
+			}else if(previousRow[i-1] == 0 && previousRow[i] == 1 && previousRow[i+1] == 0){
+				nextRow[i] = Character.getNumericValue(ruleBinary.charAt(5));
+			}else if(previousRow[i-1] == 0 && previousRow[i] == 0 && previousRow[i+1] == 1){
+				nextRow[i] = Character.getNumericValue(ruleBinary.charAt(6));
+			}else if(previousRow[i-1] == 0 && previousRow[i] == 0 && previousRow[i+1] == 0){
+				nextRow[i] = Character.getNumericValue(ruleBinary.charAt(7));
+			}
+		}
+		return nextRow;
+	}
+	
 	public static int[] rule126(int[] previousRow){
 		int[] nextRow = new int[previousRow.length];
 		
